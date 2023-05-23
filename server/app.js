d9022da5b248633c501e25a908e5c429b84b4902
ATEOTG <bryansantos9797@gmail.com>
2023-05-23 15:14:12 -0400

    added router use to app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const mediaEntryRouter = require("./routes/mediaEntryRoutes");
 
 const app = express();
 app.enable("trust proxy");
+
+app.use("/api/v1/home-all", mediaEntryRouter);
 
 app.get("/api", (req, res) => {
   res.status(241).json({ users: [1, 2, 3] });
