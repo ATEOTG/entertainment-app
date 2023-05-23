9fcc23af4eb512d79a93e813f602590717d88f5d
ATEOTG <bryansantos9797@gmail.com>
2023-05-22 20:16:38 -0400

    added mongoose DB

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const app = express();
 app.enable("trust proxy");
 
 app.get("/api", (req, res) => {
-  //   res.status(241).json({ users: [1, 2, 3] });
+  res.status(241).json({ users: [1, 2, 3] });
 });
 
 module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,21 @@
 const app = require("./app");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("DB Connection successful"));
 
 const port = process.env.PORT || 5000;
 
