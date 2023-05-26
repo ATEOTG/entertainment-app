832a873b470fd2033564b79f3b292bdd6d99cdc4
ATEOTG <bryansantos9797@gmail.com>
2023-05-26 18:10:42 -0400

    added route for updating bookmarked field

--- server/routes/mediaEntryRoutes.js
+++ server/routes/mediaEntryRoutes.js
@@ -4,7 +4,9 @@ const mediaEntryController = require("../controllers/mediaEntryController");
 const router = express.Router();
 
 router.route("/").get(mediaEntryController.getAllMediaEntries);
+router.route("/:id").patch(mediaEntryController.updateMediaBookmark);
 router.route("/trending").get(mediaEntryController.getTrendingEntries);
 router.route("/movies").get(mediaEntryController.getAllMovieMediaEntries);
 router.route("/tv").get(mediaEntryController.getAllTvMediaEntries);
+
 module.exports = router;
