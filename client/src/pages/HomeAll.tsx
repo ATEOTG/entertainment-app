f9f3a8912d9a2d2fe35171ebccfc858e067358f4
ATEOTG <bryansantos9797@gmail.com>
2023-05-27 14:12:59 -0400

    removed uneccessary import

--- client/src/pages/HomeAll.tsx
+++ client/src/pages/HomeAll.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect } from "react";
+import { Fragment } from "react";
 import EntryComponent from "../components/EntryComponent";
 import Trending from "../components/Trending/Trending";
 import { HomeProps } from "../interface";
