0ad233899623f2a0bd049461c0c46d79f2fef692
ATEOTG <bryansantos9797@gmail.com>
2023-05-30 14:30:16 -0400

    added routes,model, and controller  for user functionality

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mediaEntryRouter = require("./routes/mediaEntryRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 app.enable("trust proxy");
 app.use(express.json());
 
 app.use("/api/v1/home", mediaEntryRouter);
+app.use("/api/v1/users-media", userRouter);
 
 module.exports = app;
---
+++ server/controllers/userController.js
@@ -0,0 +1,47 @@
+const User = require("../models/userModel");
+
+exports.getAllUsers = async (req, res, next) => {
+  try {
+    const data = await User.find({});
+
+    res.status(200).json({
+      status: "success",
+      data,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.signup = async (req, res, next) => {
+  try {
+    const newUser = await User.create(req.body);
+
+    res.status(200).json({
+      status: "success",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      next(new Error("Please provide email and password"));
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user || !(await user.correctPassword(password, user.password))) {
+      return next(new Error("Incorrect email or password"));
+    }
+
+    res.status(200).json({
+      status: "successfully logged in",
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
