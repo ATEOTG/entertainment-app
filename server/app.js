600bb2ae47b41414f3114e7baf448498895678ec
ATEOTG <bryansantos9797@gmail.com>
2023-05-31 21:54:56 -0400

    added global error handler

--- server/app.js
+++ server/app.js
@@ -9,4 +9,14 @@ app.use(express.json());
 app.use("/api/v1/home", mediaEntryRouter);
 app.use("/api/v1/users-media", userRouter);
 
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
 module.exports = app;
