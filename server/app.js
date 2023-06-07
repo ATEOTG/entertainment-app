473189402e81cd07acaaa2d8da107022be6fc0d1
ATEOTG <bryansantos9797@gmail.com>
2023-06-07 16:22:40 -0400

    added data sanitation

--- server/app.js
+++ server/app.js
@@ -1,17 +1,25 @@
 const express = require("express");
-const mediaEntryRouter = require("./routes/mediaEntryRoutes");
-const userRouter = require("./routes/userRoutes");
 const cors = require("cors");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const compression = require("compression");
+
+const mediaEntryRouter = require("./routes/mediaEntryRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
+
 app.enable("trust proxy");
 app.use(cors());
 app.options("*", cors());
 app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
+app.use(compression());
+app.use(mongoSanitize());
+app.use(xss());
 
 app.use("/api/v1/entries", mediaEntryRouter);
 app.use("/api/v1/users", userRouter);
