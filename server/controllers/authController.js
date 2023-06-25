fd2adedadc981416d848f0dbc6ad681bf09a31f8
ATEOTG <bryansantos9797@gmail.com>
2023-06-25 10:46:38 -0400

    updated res for logout resource

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -91,6 +91,10 @@ exports.logout = (req, res) => {
     secure: true,
   });
 
+  res.setHeader(
+    "Cache-Control",
+    "no-cache, no-store, max-age=0, must-revalidate"
+  );
   res.status(200).json({
     status: "success",
   });
